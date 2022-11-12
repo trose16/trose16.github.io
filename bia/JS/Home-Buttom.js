bf12cd8a6414514c343c05cc89b0b3f867aa9686
Teako Warfield-Graham <teakowg@gmail.com>
2022-11-11 17:08:14 -0700

    Adds Bia

---
+++ bia/JS/Home-Buttom.js
@@ -0,0 +1,17 @@
+<div class="header">
+    <nav>
+        <img class="logo" src="TM_Logo.png" alt="tech-moms-logo">
+            <b style="font-size:20px">
+
+                <p style="text-align: right">
+
+                    <a href="https://tech-moms.org">Home Tech-Moms</a>
+
+                    <a href="javascript: document.body.scrollIntoView(false);">Scroll to bottom</a>
+                </p>
+            </ul>
+        </b>
+
+    </nav >
+
+</div >
---
+++ bia/JS/app.js
@@ -0,0 +1,22 @@
+<script type="text/javascript">
+        var blink = document.getElementById('blink');
+        setInterval(function () {
+            blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
+        }, 1500);
+    </script>
+
+    <hr width="400px">
+    <br><br><br><br>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TimelineMax.min.js"
+        integrity="sha512-lJDBw/vKlGO8aIZB8/6CY4lV+EMAL3qzViHid6wXjH/uDrqUl+uvfCROHXAEL0T/bgdAQHSuE68vRlcFHUdrUw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TimelineMax.min.js"
+        integrity="sha512-lJDBw/vKlGO8aIZB8/6CY4lV+EMAL3qzViHid6wXjH/uDrqUl+uvfCROHXAEL0T/bgdAQHSuE68vRlcFHUdrUw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="app.js">
+
+    </script>
+
